b9858d60b900ad73c115ac58d86c0757e224528e
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-07-21 20:39:52 -0300

    feat: criando login com google

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,7 +1,13 @@
+'use client';
+
+
+import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 
 
 export function Header() {
+    const { data: session, status } = useSession();
+
     return (
         <header className="bg-[#0F0F0F] text-white fixed top-0 left-0 w-full z-50 shadow-md">
             <div className="max-w-screen-xl mx-auto px-6 py-8">
@@ -12,14 +18,26 @@ export function Header() {
                             <h1 className="font-bold text-3xl cursor-pointer">Tarefas<span className="text-red-600">+</span></h1>
                         </Link>
 
-                        <Link href="/dashboard" className="text-md text-black font-semibold cursor-pointer bg-[#FAFAFA] px-4 py-1 rounded-md transition-transform duration-300 hover:scale-105">  
+                        { session?.user && (
+                            <Link href="/dashboard" className="text-md text-black font-semibold cursor-pointer bg-[#FAFAFA] px-4 py-1 rounded-md transition-transform duration-300 hover:scale-105">  
                             Meu painel
                         </Link>
+                        )}
                     </div>
 
-                    <a className="text-sm border border-amber-50 rounded-3xl px-6 py-2 cursor-pointer transition-transform duration-300 hover:scale-105">
-                        Minha conta
-                    </a>
+                    { status === "loading" ?(
+                        <></>
+                    ): session ? (
+                        <button onClick={() => {
+                            signOut();
+                        }} className="text-sm border border-amber-50 rounded-3xl px-6 py-2 cursor-pointer transition-transform duration-300 hover:scale-105">
+                        OLá {session?.user?.name}
+                    </button>
+                    ): (
+                        <button onClick={() => signIn("google")} className="text-sm border border-amber-50 rounded-3xl px-6 py-2 cursor-pointer transition-transform duration-300 hover:scale-105">
+                            Entrar com Google
+                        </button>
+                    )}
                 </nav>
             </div>
         </header>
