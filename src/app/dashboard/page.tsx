06449afa4abb4a49d9b5a317bf1cfa61eba8d71f
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-07-21 21:11:54 -0300

    configurando autorização do usuário logado para o dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,22 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { redirect } from "next/navigation";
 import Head from "next/head";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        redirect("/"); 
+    }
+
     return (
-        <main>
+        <main className="bg-[#0F0F0F] text-white min-h-screen flex flex-col items-center justify-center">
             <Head>
                 <title>Meu painel de tarefas</title>
             </Head>
-
-            <h1>Pagina painel</h1>
-
+            <h1>Página painel</h1>
+            <p className="text-xl font-semibold">Bem-vindo, {session?.user?.name}</p>
         </main>
     );
 }
